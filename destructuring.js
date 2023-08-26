a9429c334c363e4d3a3a4d322aaadaedaf2d9b8a
rahulitme <118755562+rahulitme@users.noreply.github.com>
2023-08-26 15:16:17 +0530

    Add files via upload

---
+++ destructuring.js
@@ -0,0 +1,17 @@
+<script>
+//  let[a,b,c]=[1,2,4]  
+//  console.log(a,b,c);
+  
+  
+  
+// let arr = [3, 4, 5, 9, 6, 4]
+// let [a, b, c, d,..rest]=arr
+// console.log(a, b, c,d, rest)
+
+1 let arr = [3, 5, 8, 9, 12, 14]
+2 let [a, b, c, d, ...rest] =arr
+3 // No need to do this:
+4 // let a = arr [0]
+5 // let b = arr[1] log(...data: any[]): void
+6 console.log(a, b, c, d, rest);
+</script>
